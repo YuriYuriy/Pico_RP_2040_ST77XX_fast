5c4ba7c81c4edaea824b15b6c9c05096712bfea2
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-17 20:05:50 +0300

    Update st77xx.h

--- st77xx/st77xx.h
+++ st77xx/st77xx.h
@@ -1,5 +1,19 @@
 //----------------------------------------------------------------------------------------------------
-// Lib by YuriYuriy 2025
+// Program to work with an SD card for Petit FatFS
+// Copyright (C) 2025 YuriYuri (Yuriy Golovin)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 //----------------------------------------------------------------------------------------------------
 #ifndef _ST77XX
 #define _ST77XX
@@ -87,5 +101,6 @@ void st77xx_send_command(uint8_t st77xx_command);
 void st77xx_send_data(uint8_t st77xx_data);
 void st77xx_init(void);
 void st77xx_set_col_row_addr(uint16_t st77xx_x_start, uint16_t st77xx_y_stasrt, uint16_t st77xx_x_stop, uint16_t st77xx_y_stop);
+
 
 #endif
