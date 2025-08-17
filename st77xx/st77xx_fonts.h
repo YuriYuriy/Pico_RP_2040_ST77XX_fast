908e91fcb55bc1d7505dc83019a6af853fe5917b
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-17 20:07:31 +0300

    Update st77xx_fonts.h

--- st77xx/st77xx_fonts.h
+++ st77xx/st77xx_fonts.h
@@ -1,5 +1,19 @@
 //----------------------------------------------------------------------------------------------------
-// Lib by YuriYuriy 2025
+// Program to work with an SD card for Petit FatFS
+// Copyright (C) 2025 YuriYuri (Yuriy Golovin)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 //----------------------------------------------------------------------------------------------------
 #ifndef _ST77XX_FONTS
 #define _ST77XX_FONTS
@@ -177,5 +191,6 @@ static const unsigned char fonts_7x10[] = {
 
 void st77xx_print_symbol(uint8_t symbol_x, uint8_t symbol_y, uint16_t symbol, uint16_t color_symbol, uint16_t color_background_symbol);
 void st77xx_print_line(uint8_t line_x, uint8_t line_y, unsigned char *line, uint16_t color_line, uint16_t color_background_line);
+
 
 #endif
