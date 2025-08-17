fc8fab27cf35095191ac7685118a044c57508780
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-17 20:07:09 +0300

    Update st77xx_img.h

--- st77xx/st77xx_img.h
+++ st77xx/st77xx_img.h
@@ -1,3 +1,20 @@
+//----------------------------------------------------------------------------------------------------
+// Program to work with an SD card for Petit FatFS
+// Copyright (C) 2025 YuriYuri (Yuriy Golovin)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------------------------------
 #ifndef _ST77XX_IMG
 #define _ST77XX_IMG
 
@@ -326,4 +343,5 @@ static const uint16_t image_data_Asuka[22500] = {
 };
 
 #endif
+
 
