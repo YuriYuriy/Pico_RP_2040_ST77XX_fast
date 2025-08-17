9fa3aa9b6dd8957f8839f3d62fabc1ffa6752d7c
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-17 20:06:23 +0300

    Update st77xx_graph.h

--- st77xx/st77xx_graph.h
+++ st77xx/st77xx_graph.h
@@ -1,5 +1,19 @@
 //----------------------------------------------------------------------------------------------------
-// Lib by YuriYuriy 2025
+// Program to work with an SD card for Petit FatFS
+// Copyright (C) 2025 YuriYuri (Yuriy Golovin)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 //----------------------------------------------------------------------------------------------------
 #ifndef _ST77XX_GRAPH
 #define _ST77XX_GRAPH
@@ -18,5 +32,6 @@ void st77xx_draw_circle(uint16_t radius_circle, uint16_t color_circle);
 void st77xx_draw_heart(uint16_t heart_color);
 void st77xx_draw_curveLissajous(uint16_t curveLissajous_color);
 void st77xx_draw_img(uint16_t img_x0, uint16_t img_y0, uint16_t img_x1, uint16_t img_y1, uint16_t *img_buff);
+
 
 #endif
