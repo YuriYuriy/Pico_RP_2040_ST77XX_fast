a0b3d3b5aaf2a7997711a60cf9996f67e50da943
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-19 12:24:58 +0300

    Update st77xx_img.h

--- st77xx/st77xx_img.h
+++ st77xx/st77xx_img.h
@@ -1,19 +1,5 @@
 //----------------------------------------------------------------------------------------------------
-// Program to work with an display ST7735 and ST7789
-// Copyright (C) 2025 YuriYuri (Yuriy Golovin)
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//(C) 2025 YuriYuri (Yuriy Golovin)
 //----------------------------------------------------------------------------------------------------
 #ifndef _ST77XX_IMG
 #define _ST77XX_IMG
@@ -343,6 +329,7 @@ static const uint16_t image_data_Asuka[22500] = {
 };
 
 #endif
+
 
 
 
