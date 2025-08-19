5f2fcb4e022298086fe0c0ae8265100c31a5c7f0
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-19 12:24:35 +0300

    Update st77xx_graph.c

--- st77xx/st77xx_graph.c
+++ st77xx/st77xx_graph.c
@@ -1,19 +1,5 @@
 //----------------------------------------------------------------------------------------------------
-// Program to work with an display ST7735 and ST7789
-// Copyright (C) 2025 YuriYuri (Yuriy Golovin)
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//(C) 2025 YuriYuri (Yuriy Golovin)
 //----------------------------------------------------------------------------------------------------
 #include "st77xx_graph.h"
 
@@ -243,5 +229,6 @@ void st77xx_draw_img(uint16_t img_x0, uint16_t img_y0, uint16_t img_x1, uint16_t
 {
   st77xx_draw_buff(img_x0, img_y0, img_x1, img_y1, img_buff, true);
 }
+
 
 
