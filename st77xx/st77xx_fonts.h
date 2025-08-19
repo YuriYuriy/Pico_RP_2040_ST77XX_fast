d8fd86fb54d9dcb0b285695d26c33874e3708987
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-19 12:24:23 +0300

    Update st77xx_fonts.h

--- st77xx/st77xx_fonts.h
+++ st77xx/st77xx_fonts.h
@@ -1,19 +1,5 @@
 //----------------------------------------------------------------------------------------------------
-// Program to work with an display ST7735 and ST7789
-// Copyright (C) 2025 YuriYuri (Yuriy Golovin)
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//(C) 2025 YuriYuri (Yuriy Golovin)
 //----------------------------------------------------------------------------------------------------
 #ifndef _ST77XX_FONTS
 #define _ST77XX_FONTS
@@ -193,4 +179,5 @@ void st77xx_print_symbol(uint8_t symbol_x, uint8_t symbol_y, uint16_t symbol, ui
 void st77xx_print_line(uint8_t line_x, uint8_t line_y, unsigned char *line, uint16_t color_line, uint16_t color_background_line);
 
 #endif
+
 
